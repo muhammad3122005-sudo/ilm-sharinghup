7a01de23b268b57b42a01bbe2602fcc783264668
muhammad3122005-sudo <muhammad3122005@gmail.com>
2026-04-29 17:50:22 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'ilmportal-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/admin.html',
+  '/icon_192.png',
+  '/icon_512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
